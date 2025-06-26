ea7adc62c2e715676f6b5ce5ea760164c92a5105
LegoCyberFesh27 <trenton@sabetti.com>
2025-06-26 14:16:30 -0400

    Add files via upload

---
+++ main.d
@@ -0,0 +1,40 @@
+main.o: /project/source/main.c /opt/devkitpro/wut/include/wut.h \
+ /opt/devkitpro/wut/include/wut_structsize.h \
+ /opt/devkitpro/wut/include/wut_types.h \
+ /opt/devkitpro/wut/include/wut_rplwrap.h \
+ /opt/devkitpro/wut/include/coreinit/screen.h \
+ /opt/devkitpro/wut/include/coreinit/thread.h \
+ /opt/devkitpro/wut/include/coreinit/alarm.h \
+ /opt/devkitpro/wut/include/coreinit/threadqueue.h \
+ /opt/devkitpro/wut/include/coreinit/time.h \
+ /opt/devkitpro/wut/include/coreinit/systeminfo.h \
+ /opt/devkitpro/wut/include/coreinit/context.h \
+ /opt/devkitpro/wut/include/coreinit/exception.h \
+ /opt/devkitpro/wut/include/coreinit/memory.h \
+ /opt/devkitpro/wut/include/coreinit/memdefaultheap.h \
+ /opt/devkitpro/wut/include/coreinit/memheap.h \
+ /opt/devkitpro/wut/include/coreinit/spinlock.h \
+ /opt/devkitpro/wut/include/coreinit/memlist.h \
+ /opt/devkitpro/wut/include/coreinit/launch.h \
+ /opt/devkitpro/wut/include/vpad/input.h \
+ /opt/devkitpro/wut/include/vpadbase/base.h
+/opt/devkitpro/wut/include/wut.h:
+/opt/devkitpro/wut/include/wut_structsize.h:
+/opt/devkitpro/wut/include/wut_types.h:
+/opt/devkitpro/wut/include/wut_rplwrap.h:
+/opt/devkitpro/wut/include/coreinit/screen.h:
+/opt/devkitpro/wut/include/coreinit/thread.h:
+/opt/devkitpro/wut/include/coreinit/alarm.h:
+/opt/devkitpro/wut/include/coreinit/threadqueue.h:
+/opt/devkitpro/wut/include/coreinit/time.h:
+/opt/devkitpro/wut/include/coreinit/systeminfo.h:
+/opt/devkitpro/wut/include/coreinit/context.h:
+/opt/devkitpro/wut/include/coreinit/exception.h:
+/opt/devkitpro/wut/include/coreinit/memory.h:
+/opt/devkitpro/wut/include/coreinit/memdefaultheap.h:
+/opt/devkitpro/wut/include/coreinit/memheap.h:
+/opt/devkitpro/wut/include/coreinit/spinlock.h:
+/opt/devkitpro/wut/include/coreinit/memlist.h:
+/opt/devkitpro/wut/include/coreinit/launch.h:
+/opt/devkitpro/wut/include/vpad/input.h:
+/opt/devkitpro/wut/include/vpadbase/base.h:
